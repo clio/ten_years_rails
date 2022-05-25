0dd8fbfe017a0893f0cde6f6e0c201d4018285bc
rishijain <jainrishi.37@gmail.com>
2022-05-25 18:38:22 +0530

    stub requests to rubygems for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require "bundler/setup"
 require "next_rails"
+
+require 'webmock/rspec'
+WebMock.disable_net_connect!(allow_localhost: true)
+
 if ENV['COVERAGE'] == 'true'
   require 'simplecov'
   SimpleCov.start do
@@ -28,5 +32,15 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.before(:each) do
+    stub_request(:get, /rubygems.org\/api\/v2\/rubygems\/rails\/versions/).
+      with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>'rubygems.org', 'User-Agent'=>'Ruby'}).
+      to_return(status: 200, body: "{\"ruby_version\": \">= 2.7.0\"}", headers: {})
+
+    stub_request(:get, /rubygems.org\/api\/v1\/versions\/rails.json/).
+      with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>'rubygems.org', 'User-Agent'=>'Ruby'}).
+      to_return(status: 200, body: "[{\"number\": \"7.0.0\"}, {\"number\": \"6.1.6\"}]", headers: {})
   end
 end
