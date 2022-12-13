21bcc2a0fac8930924c46d3b6aa26518cff8860a
Sean Dickinson <sean@bythepixel.com>
2022-12-13 08:39:33 -0500

    fix: add another spec

--- spec/next_rails/gem_info_spec.rb
+++ spec/next_rails/gem_info_spec.rb
@@ -6,15 +6,35 @@ RSpec.describe NextRails::GemInfo do
     let(:mock_gem) { Struct.new(:name, :version, :runtime_dependencies) }
     let(:mocked_dependency) { Struct.new(:name, :requirement) }
     it "returns :incompatible if gem specifies a rails dependency but no compatible version is found" do
-
+      # set up a mock gem with with a rails dependency that is unsatisfied by the version given
       mocked_dependency_requirement = double("requirement")
       allow(mocked_dependency_requirement).to receive(:satisfied_by?).and_return(false)
       runtime_deps = [mocked_dependency.new("rails", mocked_dependency_requirement)]
       incompatible_gem = mock_gem.new('incompatible', '0.0.1', runtime_deps)
+
       rails_version = "7.0.0"
       gem_info = NextRails::GemInfo.new(incompatible_gem)
 
       expect(gem_info.state(rails_version)).to eq(:incompatible)
+    end
+
+    it "returns :no_new_version if a gem specifies an unsatisfied rails dependency and no other specs are returned" do
+      # set up a mock gem with with a rails dependency that is unsatisfied by the version given
+      mocked_dependency_requirement = double("requirement")
+      allow(mocked_dependency_requirement).to receive(:satisfied_by?).and_return(false)
+      runtime_deps = [mocked_dependency.new("rails", mocked_dependency_requirement)]
+      incompatible_gem = mock_gem.new('incompatible', '0.0.1', runtime_deps)
+
+      # Set up a mock SpecFetcher to return an empty list
+      fetcher_double = double("spec_fetcher")
+      allow(fetcher_double).to receive(:available_specs).and_return([[],[]])
+      allow(Gem::SpecFetcher).to receive(:new).and_return(fetcher_double)
+
+      rails_version = "7.0.0"
+      gem_info = NextRails::GemInfo.new(incompatible_gem)
+      gem_info.find_latest_compatible
+
+      expect(gem_info.state(rails_version)).to eq(:no_new_version)
     end
 
   end
@@ -22,10 +42,12 @@ RSpec.describe NextRails::GemInfo do
   describe "#find_latest_compatible" do
     let(:mock_gem) { Struct.new(:name, :version) }
     it "sets latest_compatible_version to NullGem if no specs are found" do
+      gem = mock_gem.new('gem_name', "0.0.1")
+
+      # Set up a mock SpecFetcher to return an empty list
       fetcher_double = double("spec_fetcher")
       allow(fetcher_double).to receive(:available_specs).and_return([[],[]])
       allow(Gem::SpecFetcher).to receive(:new).and_return(fetcher_double)
-      gem = mock_gem.new('gem_name', "0.0.1")
 
       gem_info = NextRails::GemInfo.new(gem)
       gem_info.find_latest_compatible
