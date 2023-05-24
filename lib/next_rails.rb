ac16f14a78159790c868aaf67f8a2cf76f6de317
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2023-05-24 12:01:30 -0400

    Had to add a new method to make the tests pass

--- lib/next_rails.rb
+++ lib/next_rails.rb
@@ -6,9 +6,19 @@ require "next_rails/bundle_report"
 require "deprecation_tracker"
 
 module NextRails
-  extend self
 
-  def next?
-    ENV["BUNDLE_GEMFILE"] == "Gemfile.next"
+  # This method will check your environment
+  # (e.g. `ENV['BUNDLE_GEMFILE]`) to determine whether your application is
+  # running with the next set of dependencies or the current set of dependencies.
+  #
+  # @return [Boolean]
+  def self.next?
+    @@next_bundle_gemfile ||= ENV["BUNDLE_GEMFILE"] == "Gemfile.next"
+  end
+
+  # This method will reset the @@next_bundle_gemfile variable. Then next time
+  # you call `NextRails.next?` it will check the environment once again.
+  def self.reset_next_bundle_gemfile
+    @@next_bundle_gemfile = nil
   end
 end
