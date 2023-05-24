53b7cdb5f8b1606b4c3fd98d0bdf9317238e4230
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2023-05-24 10:49:58 -0400

    Add basic implementation of the next? method

--- lib/next_rails.rb
+++ lib/next_rails.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
 require "next_rails/gem_info"
 require "next_rails/version"
 require "next_rails/bundle_report"
 require "deprecation_tracker"
 
 module NextRails
-  # Your code goes here...
+  extend self
+
+  def next?
+    ENV["BUNDLE_GEMFILE"] == "Gemfile.next"
+  end
 end
